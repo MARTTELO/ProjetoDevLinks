3e8be796beb6f02ea2066f7d5d3613b0b084d4f1
MARTTELO <marttelo@gmail.com>
2023-11-15 03:33:05 -0300

    refactore link assets

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function toggleMode() {
   const img = document.querySelector("#profile img");
 
    if( html.classList.contains("light")){
-    img.setAttribute("src", "/assets/avatar-light.png");
+    img.setAttribute("src", "assets/avatar-light.png");
     img.setAttribute("alt", "avatar Marttelo Light");
    }else{
-    img.setAttribute("src", "/assets/avatar.png");
+    img.setAttribute("src", "assets/avatar.png");
     img.setAttribute("alt", "Foto Marttelo")
    }
 
